8bc411281e5c0f151102c4b652d14a7891e77a33
OhanesianRostislav <128744686+OhanesianRostislav@users.noreply.github.com>
2024-06-04 00:46:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,4 +53,63 @@ window.addEventListener("scroll", function () {
     else {
         header.classList.remove("active");
     }
-})
+});
+/*Hero slider*/
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+    lastActiveSliderItem.classList.remove("active");
+    heroSliderItems[currentSlidePos].classList.add("active");
+    lastActiveSliderItem = heroSliderItems[currentSlidePos];
+}
+
+const slideNext = function () {
+    if (currentSlidePos >= heroSliderItems.length - 1) {
+        currentSlidePos = 0;
+    }
+    else {
+        currentSlidePos++;
+    }
+
+    updateSliderPos();
+}
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+        currentSlidePos = heroSLiderItems.length - 1; 
+    }
+    else {
+        currentSlidePos--;
+    }
+
+    updateSliderPos();
+}
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+    
+    
+
+/*auto slide*/
+let autoSlideInterval; 
+
+const autoSlide = function () {
+    autoSlideInterval = setInterval(function () {
+        slideNext();
+    }, 7000);
+}
+
+addEventOneElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function () {
+    clearInterval(autoSlideInterval);
+});
+
+addEventOneElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
+
+window.addEventListener("load", autoSlide);
